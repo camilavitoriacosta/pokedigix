5c7c85612bd17d305216af7ac5cde94c64651d8b
camilacosta <camilavitoria.c.costa@gmail.com>
2022-07-22 16:00:00 -0400

    Tipo - Adicionando metodo para buscar tipo por nome

--- src/main/java/br/com/digix/pokedigix/Tipo/TipoRepository.java
+++ src/main/java/br/com/digix/pokedigix/Tipo/TipoRepository.java
@@ -1,9 +1,9 @@
 package br.com.digix.pokedigix.tipo;
 
-import org.springframework.data.jpa.repository.Query;
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface TipoRepository extends CrudRepository<Tipo, Long> {
-    // @Query("select t from tipo t where nome = ?1")
-    // Tipo findByName(String nome);
+public interface TipoRepository extends CrudRepository<Tipo, Long> {    
+    Collection<Tipo> findByNome(String nome);
 }
--- src/test/java/br/com/digix/pokedigix/tipo/TipoRepositoryTest.java
+++ src/test/java/br/com/digix/pokedigix/tipo/TipoRepositoryTest.java
@@ -1,6 +1,9 @@
 package br.com.digix.pokedigix.tipo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collection;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,29 +11,28 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest // indica conexao com o banco
 public class TipoRepositoryTest {
-    
+
     @Autowired // spring gera as dependencias automaticamente
     private TipoRepository tipoRepository;
 
     @Test
-    public void deve_salvar_um_tipo(){
+    public void deve_salvar_um_tipo() {
         String nomeEsperado = "Eletrico";
-        Long idEsperado = 4L;
         Tipo tipo = new Tipo(nomeEsperado);
 
         tipoRepository.save(tipo);
 
-        assertEquals(idEsperado, tipo.getId());
+        assertNotNull(tipo.getId());
     }
-  
-    // @Test
-    // public void deve_procurar_um_tipo_por_nome(){
-    //     String nome = "Eletrico";
-    //     Tipo tipo = new Tipo(nome);
-    //     tipoRepository.save(tipo);
 
-    //     var tipoRetornado = tipoRepository.findByName(nome);
+    @Test
+    public void deve_procurar_um_tipo_por_nome() {
+        String nome = "Eletrico";
+        Tipo tipo = new Tipo(nome);
+        tipoRepository.save(tipo);
 
-    //     assertEquals(tipo.getId(), tipoRetornado.getId());
-    // }
+        Collection<Tipo> tiposRetornados = tipoRepository.findByNome(nome);
+
+        assertTrue(tiposRetornados.contains(tipo));
+    }
 }
