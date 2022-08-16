6fc76ff3adcfe44d7cb4ab926d4132c0ca8799bd
camilacosta <camilavitoria.c.costa@gmail.com>
2022-08-16 13:37:19 -0400

    criando recurso deletar no Tipo

--- src/main/java/br/com/digix/pokedigix/Tipo/TipoController.java
+++ src/main/java/br/com/digix/pokedigix/Tipo/TipoController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +38,11 @@ public class TipoController {
     public TipoResponseDTO buscarPorId(@PathVariable Long id) {
         Tipo tipoRetornado = tipoRepository.findById(id).get();
         return new TipoResponseDTO(tipoRetornado.getId(), tipoRetornado.getNome());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletarPorId(@PathVariable Long id) {
+        tipoRepository.deleteById(id);
     }
 
 }
